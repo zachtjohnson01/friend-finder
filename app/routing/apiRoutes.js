e9a48d21b53bbfa1f10563d5605c6e799155971d
Zach Johnson <zachtjohnson01@gmail.com>
2018-03-04 20:45:20 -0600

    Completed survey input functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,10 @@ module.exports = function(app) {
     app.get("/api/questions", function(req, res) {
         res.json(questionsData);
     });
+    app.get("/api/surveyResults", function(req, res) {
+        res.json(friendsData);
+    });
+    app.post("/api/surveyResults", function(req,res) {
+        friendsData.push(req.body);
+    });
 };
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 3000;
 var apiRoutes = require("./app/routing/apiRoutes");
 var htmlRoutes = require("./app/routing/htmlRoutes");
 
-
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
