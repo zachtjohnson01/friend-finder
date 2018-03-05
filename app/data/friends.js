4c39838e631caa3cf88f784b4d31140acb1db436
Zach Johnson <zachtjohnson01@gmail.com>
2018-03-04 22:17:42 -0600

    Added initial comparison logic

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,34 @@ var friendsArray = [
     {
         friendId: 1,
         name: "Zach Johnson",
-        quizAnswers: []
+        quizAnswers: [
+            "5",
+            "5",
+            "3",
+            "4",
+            "1",
+            "2",
+            "5",
+            "4",
+            "5",
+            "5"
+        ]
+    },
+    {
+        friendId: 2,
+        name: "Alex Johnson",
+        quizAnswers: [
+            "5",
+            "4",
+            "1",
+            "2",
+            "4",
+            "5",
+            "5",
+            "1",
+            "5",
+            "3"
+        ]
     }
 ];
 
